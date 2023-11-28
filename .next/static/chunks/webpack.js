f55bdc0cd81e9912303d3f883650565c6074a94a
JPivista <jpivistasolutions@gmail.com>
2023-11-28 09:40:11 +0530

    footer form integration

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d8e9622b9f0043e8"; }
+/******/ 		__webpack_require__.h = function() { return "26f7edf34982e667"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
